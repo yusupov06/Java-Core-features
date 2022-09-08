16d0c3327f1f09459a56288f28eb26a427d3dae8
yusupov06 <yusupovforwin@gmail.com>
2022-09-08 23:19:01 +0500

    initial commit

---
+++ src/main/java/uz/md/Main.java
@@ -0,0 +1,11 @@
+package uz.md;
+
+/**
+ * Me: ${USER}
+ * Project: ${PROJECT_NAME}/${PRODUCT_NAME}
+ * Date:${DAY_NAME_SHORT} ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
